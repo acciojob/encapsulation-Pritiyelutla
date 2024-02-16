64c0955f23b23b1e57f3c3987b3e329cf525f0fb
Prit <pritiyelutla@gmail.com>
2024-02-16 14:42:51 +0530

    Task done 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main ( String args[] )
+    {
+        RWOnly obj = new RWOnly();
+
+        //obj.name = "Ammu";
+        //System.out.println(obj.name);
+        // 'name' has private access in 'com.driver.RWOnly'
+
+        obj.setName("Priti");
+        System.out.println(obj.getName());
+    }
 }
